a8e74364a08e5446fe9066c7dda705f37fbacb6b
David Siegel <djsiegel@gmail.com>
2017-03-27 09:54:13 -0700

    Skip node download/install if present

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,33 +26,42 @@ function wait {
     read -rsp $'Press any key to continue...' -n1 key
 }
 
-
 focus
 echo "Hello!"
 echo
 echo "Let's install everything you need to use Mobile Center."
-wait
-
-focus
-echo "First, we'll download some prerequisites (nodejs)..."
 echo
-curl -O $NODE_URL
-
-focus
-echo "We've downloaded everything we need!"
-echo
-echo "Now let's install the mobile-center command."
-wait
-
-focus
 echo "Please type your account password (used to unlock your Mac):"
 echo
-sudo echo "Installing nodejs..."
-sudo installer -verboseR -pkg $NODE_PKG -target / > /dev/null
-rm -rf $NODE_PKG
+sudo echo
 
-echo "Installing mobile-center command line tool..."
-sudo npm install -g --loglevel silent mobile-center-bootstrap > /dev/null
+download_node() {
+    focus
+    echo "First, we'll download some prerequisites (nodejs)..."
+    echo
+    curl -O $NODE_URL
+}
+
+install_node() {
+    echo "Installing nodejs..."
+    sudo installer -verboseR -pkg $NODE_PKG -target / > /dev/null
+    rm -rf $NODE_PKG
+}
+
+install_npm_deps() {
+    focus
+    echo "Initializing mobile-center command line tool..."
+    sudo npm install -g --silent --progress=false mobile-center-bootstrap > /dev/null
+}
+
+if hash node 2>/dev/null; then
+    echo
+else
+    download_node
+    install_node
+fi
+
+install_npm_deps
 
 focus
 mobile-center-bootstrap
